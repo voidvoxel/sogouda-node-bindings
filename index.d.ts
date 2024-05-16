2376befc873709f496939e8ba1bd3fbdab22a6a4
ashlynn-juniper <160202125+voidvoxel@users.noreply.github.com>
2024-05-16 12:15:27 -0500

    Build type mappings

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+export default interface StartOptions {
+    title: string;
+    url: string;
+    width: number;
+    height: number;
+    frameless: boolean;
+    debug: boolean;
+}
+/**
+ * Start a Sogouda app.
+ *
+ * @param {StartOptions} options
+ * @returns {any}
+ */
+export declare function start(options: StartOptions): void;
+//# sourceMappingURL=index.d.ts.map
