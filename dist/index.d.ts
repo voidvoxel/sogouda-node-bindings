99aeef3361922ab5a784eefb918ff5c6bd0e7fb3
voidvoxel <160202125+voidvoxel@users.noreply.github.com>
2024-05-20 00:22:04 -0400

    Remove `dist/` from repository

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare let bindings: any;
-export declare function load(): Promise<any>;
-export declare function start(...args: any[]): Promise<any>;
-export declare function startSync(...args: any[]): any;
-export default bindings;
